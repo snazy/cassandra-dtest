9749c78b6a77ee730720ee44b08cd8fe3d9828ae
Sean McCarthy <spm64@pitt.edu>
2016-09-28 12:02:10 -0400

    additional supercolumn coverage
    pep8 clean
    
    remove unused imports
    
    move supercolumn tests to upgrade_tests dir. Define supercolumn schema in test method. Review fixes
    
    track deletes
    
    point to correct directory. Test is executed from dtest root folder.
    
    describe data in sstables

--- upgrade_tests/upgrade_supercolumns_test.py
+++ upgrade_tests/upgrade_supercolumns_test.py
@@ -1,8 +1,13 @@
+import os
 import time
+
+from collections import OrderedDict
 
 from flaky import flaky
 
 from dtest import CASSANDRA_VERSION_FROM_BUILD, Tester, debug
+from pycassa.pool import ConnectionPool
+from pycassa.columnfamily import ColumnFamily
 from thrift_bindings.v22.ttypes import \
     ConsistencyLevel as ThriftConsistencyLevel
 from thrift_bindings.v22.ttypes import (CfDef, Column, ColumnOrSuperColumn,
@@ -10,11 +15,18 @@ from thrift_bindings.v22.ttypes import (CfDef, Column, ColumnOrSuperColumn,
                                         Mutation, SlicePredicate, SliceRange,
                                         SuperColumn, TimedOutException)
 from thrift_tests import get_thrift_client
-from tools.decorators import known_failure, since
+from tools.assertions import assert_all
+from tools.decorators import known_failure
 from tools.flaky import RerunTestException, requires_rerun
 
 
-@since('2.0', max_version='2.1.x')
+# Use static supercolumn data to reduce total test time and avoid driver issues connecting to C* 1.2.
+# The data contained in the SSTables is (name, {'attr': {'name': name}}) for the name in NAMES.
+SCHEMA_PATH = os.path.join("./", "upgrade_tests", "supercolumn-data", "cassandra-2.0", "schema-2.0.cql")
+TABLES_PATH = os.path.join("./", "upgrade_tests", "supercolumn-data", "cassandra-2.0", "supcols", "cols")
+NAMES = ["Alice", "Bob", "Claire", "Dave", "Ed", "Frank", "Grace"]
+
+
 class TestSCUpgrade(Tester):
     """
     Tests upgrade between 1.2->2.0 for super columns (since that's where we
@@ -37,6 +49,87 @@ class TestSCUpgrade(Tester):
             self.ignore_log_patterns += [_known_teardown_race_error]
 
         Tester.__init__(self, *args, **kwargs)
+
+    def prepare(self, num_nodes=1, cassandra_version="git:cassandra-2.1"):
+        cluster = self.cluster
+
+        # Forcing cluster version on purpose
+        cluster.set_install_dir(version=cassandra_version)
+        if "memtable_allocation_type" in cluster._config_options:
+            del cluster._config_options['memtable_allocation_type']
+        cluster.populate(num_nodes).start()
+
+        return cluster
+
+    def verify_with_thrift(self):
+        pool = ConnectionPool("supcols", pool_size=1)
+        super_col_fam = ColumnFamily(pool, "cols")
+        for name in NAMES:
+            super_col_value = super_col_fam.get(name)
+            self.assertEqual(OrderedDict([(('attr', u'name'), name)]), super_col_value)
+
+    def verify_with_cql(self, session):
+        session.execute("USE supcols")
+        expected = [[name, 'attr', u'name', name] for name in ['Grace', 'Claire', 'Dave', 'Frank', 'Ed', 'Bob', 'Alice']]
+        assert_all(session, "SELECT * FROM cols", expected)
+
+    def _upgrade_super_columns_through_versions_test(self, upgrade_path):
+        cluster = self.prepare()
+        node1 = cluster.nodelist()[0]
+        node1.run_cqlsh(cmds="""CREATE KEYSPACE supcols WITH replication = {
+                                    'class': 'SimpleStrategy',
+                                    'replication_factor': '1'
+                                  };
+
+                                USE supcols;
+
+                                CREATE TABLE cols (
+                                  key blob,
+                                  column1 blob,
+                                  column2 text,
+                                  value blob,
+                                  PRIMARY KEY ((key), column1, column2)
+                                ) WITH COMPACT STORAGE AND
+                                  bloom_filter_fp_chance=0.010000 AND
+                                  caching='KEYS_ONLY' AND
+                                  comment='' AND
+                                  dclocal_read_repair_chance=0.000000 AND
+                                  gc_grace_seconds=864000 AND
+                                  index_interval=128 AND
+                                  read_repair_chance=0.100000 AND
+                                  replicate_on_write='true' AND
+                                  populate_io_cache_on_flush='false' AND
+                                  default_time_to_live=0 AND
+                                  speculative_retry='99.0PERCENTILE' AND
+                                  memtable_flush_period_in_ms=0 AND
+                                  compaction={'class': 'SizeTieredCompactionStrategy'} AND
+                                  compression={'sstable_compression': 'SnappyCompressor'};
+                              """)
+        node1.bulkload(options=[TABLES_PATH])
+        node1.nodetool("upgradesstables -a")
+
+        session = self.patient_exclusive_cql_connection(node1)
+
+        self.verify_with_cql(session)
+        self.verify_with_thrift()
+
+        for version in upgrade_path:
+            self.upgrade_to_version(version)
+            node1.nodetool("enablethrift")
+
+            session = self.patient_exclusive_cql_connection(node1)
+
+            self.verify_with_cql(session)
+            self.verify_with_thrift()
+
+        cluster.remove(node=node1)
+
+    def upgrade_super_columns_through_all_versions_test(self):
+        self._upgrade_super_columns_through_versions_test(upgrade_path=['git:cassandra-2.2', 'git:cassandra-3.0',
+                                                                        'git:cassandra-3.9', 'git:trunk'])
+
+    def upgrade_super_columns_through_limited_versions_test(self):
+        self._upgrade_super_columns_through_versions_test(upgrade_path=['git:cassandra-3.0', 'git:trunk'])
 
     @known_failure(failure_source='test',
                    jira_url='https://issues.apache.org/jira/browse/CASSANDRA-12616',
@@ -298,7 +391,7 @@ class TestSCUpgrade(Tester):
             node.set_install_dir(version=tag)
             if tag < "2.1":
                 if "memtable_allocation_type" in node.config_options:
-                    node.config_options.__delitem__("memtable_allocation_type")
+                    del node.config_options['memtable_allocation_type']
             debug("Set new cassandra dir for %s: %s" % (node.name, node.get_install_dir()))
         self.cluster.set_install_dir(version=tag)
 
