1a324bdef600e2431d09cbf8e31b86c1a5f4faa4
Philip Thompson <ptnapoleon@gmail.com>
2017-02-09 10:11:10 -0500

    Add more debug output to sstabledump test

--- offline_tools_test.py
+++ offline_tools_test.py
@@ -406,7 +406,10 @@ class TestOfflineTools(Tester):
         try:
             [(out, error, rc)] = node1.run_sstabledump(keyspace='ks', column_families=['cf'])
         except Exception:
-            debug(node1.run_sstabledump(keyspace='ks', column_families=['cf']))
+            for out, error, rc in node1.run_sstabledump(keyspace='ks', column_families=['cf'])
+                debug(json.loads(out))
+                debug(error)
+                debug(rc)
             self.fail("More than one sstable found.")
         debug(out)
         debug(error)
