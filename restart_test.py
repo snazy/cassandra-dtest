29273d5e708949621a0ce1eb4b2ddd6842c44af6
Dimitar Dimitrov <30328539+dimitarndimitrov@users.noreply.github.com>
2018-01-04 13:54:24 +0200

    Introduce a test checking schema changes during abrupt node restart
    patch by Dimitar Dimitrov; reviewed by Stefania Alborghetti for DB-1487

---
+++ restart_test.py
@@ -0,0 +1,41 @@
+import time
+
+from dtest import Tester
+from tools.data import create_ks
+from tools.decorators import since
+
+
+@since('3.0')
+class TestRestart(Tester):
+    def schema_change_while_node_initializing_test(self):
+        """
+        Tests whether a slow initialization of a node's StorageService/Gossiper after restart can lead to problems
+        if there are schema changes in the meantime.
+        @jira_ticket DB-1487
+        """
+        cluster = self.cluster
+        cluster.populate(2, debug=True, install_byteman=True).start()
+
+        node2 = cluster.nodelist()[1]
+        # Stop a node without waiting for the others to notice (in order for them to still consider the node alive
+        # during its initialization, where is the vulnerability window we're testing)
+        node2.stop(wait_other_notice=False, gently=False)
+        # Start without waiting, in order to trigger the bytecode manipulation in time to affect the startup code
+        node2.start(no_wait=True, wait_other_notice=False, wait_for_binary_proto=False)
+        time.sleep(1)
+        node2.byteman_submit(['./byteman/delay_storage_service_init.btm'])
+
+        # This sleep interval should be as short as possible, but long enough to have the tested node restart.
+        # Its purpose is to allow the schema change load that follows it to happen as early as possible, before
+        # the healthy node is able to pick up that the tested node has restarted, and may not be in working condition
+        time.sleep(1)
+
+        node1 = cluster.nodelist()[0]
+        session = self.patient_cql_connection(node1)
+        for i in range(1, 10):
+            # Trigger a schema change (that needs to reach the tested node)
+            create_ks(session, 'test_keyspace' + str(i), 2)
+            time.sleep(1)
+
+        # Wait enough to have the test complete after the delayed StorageService initialization has completed
+        time.sleep(10)
