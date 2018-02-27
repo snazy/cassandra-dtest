e1c53a63a68a5b12c72b337ce6f1d82e572a097c
Dimitar Dimitrov <30328539+dimitarndimitrov@users.noreply.github.com>
2018-02-27 18:31:52 +0200

    DB-1769 Fix various problems with tpc_cores_test.py. (#234)
    Fixes problems accumulated with changes to the read and write TPC task
    types, and to the output of the "nodetool tpstats --cores" command.
    Very similar to https://github.com/riptano/apollo-dtest/pull/204, also
    adds a couple of small debugging improvements.

--- tpc_cores_test.py
+++ tpc_cores_test.py
@@ -8,40 +8,52 @@ from tools.misc import new_node
 
 default_op_count = 20000
 tolerance = int(default_op_count / 20)
-read_op = 'read'
-write_op = 'write'
-write_switch_op = 'write_switch_for_memtable'
+tpc_read_task = 'read_(remote|local)'
+tpc_write_task = 'write_(remote|local)'
+tpc_write_switch_task = 'write_switch_for_memtable'
+prefix_tpc_read_tasks = 'read_'
+prefix_tpc_write_tasks = 'write_'
 
 
 @since('4.0')
 class TestTPCCores(Tester):
+    # TODO Calculate tolerance based on the currently used op_count, not based on some default
     @staticmethod
-    def _stress_and_get_tpstats(node, op, phase, op_count=default_op_count):
-        node.stress([op, 'n=' + str(op_count), 'no-warmup', '-rate', 'threads=100'])
-        out, err, _ = node.nodetool('tpstats --cores')
+    def _get_tolerance(tpc_task):
+        if re.match(prefix_tpc_read_tasks, tpc_task):
+            return default_op_count / 5
+        if re.match(prefix_tpc_write_tasks, tpc_task):
+            return default_op_count / 20
+        raise AssertionError('Expected a read or write TPC task, but got {}'.format(tpc_task))
+
+    @staticmethod
+    def _stress_and_get_tpstats(node, stress_op, phase, op_count=default_op_count):
         debug(os.linesep + '=== ' + phase + ' ===')
+        node.stress([stress_op, 'n=' + str(op_count), 'no-warmup', '-rate', 'threads=100'])
+        out, err, _ = node.nodetool('tpstats --cores')
         debug(out)
         return out
 
     @staticmethod
-    def _get_completed(out, stat):
+    def _get_all_completed(out, tpc_task):
+        all_completed = 0
+        tpc_task_pattern = re.compile('TPC/all/' + tpc_task.upper() + ' ')
         for line in out.split(os.linesep):
-            if "TPC/all/" + stat.upper() in line:
-                completed = line.split()[3]
-                return int(completed)
-        return 0
+            if re.match(tpc_task_pattern, line):
+                all_completed += int(line.split()[5])
+        return all_completed
 
-    def _assert_count_expected(self, op, actual_count, expected_count, phase):
-        upper_bound = expected_count + tolerance
+    def _assert_count_expected(self, tpc_task, actual_count, expected_count, phase):
+        upper_bound = expected_count + TestTPCCores._get_tolerance(tpc_task)
         self.assertTrue(actual_count <= upper_bound,
                         'Too many {0} operations during \'{1}\': {2!s} (Expected at most {3!s})'
-                        .format(op, phase, actual_count, upper_bound))
-        lower_bound = expected_count - tolerance
+                        .format(tpc_task, phase, actual_count, upper_bound))
+        lower_bound = expected_count - TestTPCCores._get_tolerance(tpc_task)
         self.assertTrue(actual_count >= lower_bound,
                         'Too few {0} operations during \'{1}\': {2!s} (Expected at least {3!s})'
-                        .format(op, phase, actual_count, lower_bound))
+                        .format(tpc_task, phase, actual_count, lower_bound))
         debug('Actual {0} operations during \'{1}\' ({2!s}) correctly fit in the expected range [{3!s}, {4!s}]'
-              .format(op, phase, actual_count, lower_bound, upper_bound))
+              .format(tpc_task, phase, actual_count, lower_bound, upper_bound))
 
     def tpc_startup_switch_cores_test(self):
         """
@@ -51,63 +63,63 @@ class TestTPCCores(Tester):
         already on a TPC thread, this check applies solely for writes.
         Also verify that we report the correct number of reads / writes in the TPC stats.
         """
+        debug("Creating and starting a single-node cluster")
         cluster = self.cluster
         cluster.populate(1).start()
-        node1 = cluster.nodes['node1']
+        node1 = cluster.nodelist()[0]
 
         # Check that we are not switching cores too much and that we are reporting the expected number of writes
         phase = 'initial write'
         num_writes = default_op_count
-        tpstats = self._stress_and_get_tpstats(node1, write_op, phase, num_writes)
-        self._assert_count_expected(write_switch_op, self._get_completed(tpstats, write_switch_op), 0, phase)
-        self._assert_count_expected(write_op, self._get_completed(tpstats, write_op), num_writes, phase)
+        tpstats = TestTPCCores._stress_and_get_tpstats(node1, 'write', phase, num_writes)
+        self._assert_count_expected(tpc_write_switch_task, TestTPCCores._get_all_completed(tpstats, tpc_write_switch_task), 0, phase)
+        self._assert_count_expected(tpc_write_task, TestTPCCores._get_all_completed(tpstats, tpc_write_task), num_writes, phase)
 
         # Check that we are reporting the expected number of reads
         phase = 'initial read'
         num_reads = default_op_count
-        tpstats = self._stress_and_get_tpstats(node1, read_op, phase, num_reads)
-        self._assert_count_expected(read_op, self._get_completed(tpstats, read_op), num_reads, phase)
+        tpstats = TestTPCCores._stress_and_get_tpstats(node1, 'read', phase, num_reads)
+        self._assert_count_expected(tpc_read_task, TestTPCCores._get_all_completed(tpstats, tpc_read_task), num_reads, phase)
 
-        # Abruptly restart the node (will require commit log replay after start)
+        debug(os.linesep + "Restarting a node abruptly (commit log replay will be needed)")
         node1.stop(gently=False)
         node1.start(wait_for_binary_proto=True)
 
         # Check that we are reporting the expected number of reads and write switches (the latter due to commit
         # log replay) after stopping and restarting
         phase = 'read after restart'
-        tpstats = self._stress_and_get_tpstats(node1, read_op, phase, num_reads)
-        self._assert_count_expected(read_op, self._get_completed(tpstats, read_op), num_reads, phase)
-        self._assert_count_expected(write_switch_op, self._get_completed(tpstats, write_switch_op), num_writes, phase)
+        tpstats = TestTPCCores._stress_and_get_tpstats(node1, 'read', phase, num_reads)
+        self._assert_count_expected(tpc_read_task, TestTPCCores._get_all_completed(tpstats, tpc_read_task), num_reads, phase)
+        self._assert_count_expected(tpc_write_switch_task, TestTPCCores._get_all_completed(tpstats, tpc_write_switch_task), num_writes, phase)
 
         # Check that we are not switching cores too much (i.e. that the number of switches has not changed since the
         # "read after restart" phase) and that we are reporting the expected number of writes after stopping and
         # restarting with replay
         phase = 'write after restart'
-        tpstats = self._stress_and_get_tpstats(node1, write_op, phase, num_writes)
-        self._assert_count_expected(write_switch_op, self._get_completed(tpstats, write_switch_op), num_writes, phase)
-        self._assert_count_expected(write_op, self._get_completed(tpstats, write_op), num_writes, phase)
+        tpstats = TestTPCCores._stress_and_get_tpstats(node1, 'write', phase, num_writes)
+        self._assert_count_expected(tpc_write_switch_task, TestTPCCores._get_all_completed(tpstats, tpc_write_switch_task), num_writes, phase)
+        self._assert_count_expected(tpc_write_task, TestTPCCores._get_all_completed(tpstats, tpc_write_task), num_writes, phase)
 
     @staticmethod
-    def _is_balanced_distribution(total, cores, counts, op, phase):
+    def _is_balanced_distribution(total, cores, counts, tpc_task, phase):
         expected_per_core = int(total / len(cores))
         for i in range(len(cores)):
-            upper_bound = expected_per_core + tolerance
-            lower_bound = expected_per_core - tolerance
+            upper_bound = expected_per_core + TestTPCCores._get_tolerance(tpc_task)
+            lower_bound = expected_per_core - TestTPCCores._get_tolerance(tpc_task)
             if counts[i] > upper_bound or counts[i] < lower_bound:
                 debug('Actual {0} operations during \'{1}\' ({2}) do not fit in the expected range [{3!s}, {4!s}]'
-                      .format(op, phase, counts[i], lower_bound, upper_bound))
+                      .format(tpc_task, phase, counts[i], lower_bound, upper_bound))
                 return False
         return True
 
-    def _stress_and_check_balance(self, node, op, phase, run_stress=True, op_count=default_op_count):
-        if run_stress:
-            node.stress([op, 'n=' + str(op_count), 'no-warmup', '-rate', 'threads=100'])
+    def _stress_and_check_balance(self, node, tpc_task, phase, stress_op=False, op_count=default_op_count):
+        if stress_op:
+            node.stress([stress_op, 'n=' + str(op_count), 'no-warmup', '-rate', 'threads=100'])
 
         out, err, _ = node.nodetool('tpstats --cores')
-        debug(os.linesep + '=== ' + phase + ' ===')
         debug(out)
 
-        stat = re.compile('TPC/(\d|all)/' + op.upper() + ' ')
+        stat = re.compile('TPC/(\d|all)/' + tpc_task.upper() + ' ')
         total = 0
         cores = []
         counts = []
@@ -115,9 +127,9 @@ class TestTPCCores(Tester):
         for line in out.split(os.linesep):
             match = re.match(stat, line)
             if match is not None:
-                count = int(line.split()[3])
+                count = int(line.split()[5])
                 core = match.group(1)
-                debug('{0} on core {1}: {2!s}'.format(op, core, count))
+                debug('{0} on core {1}: {2!s}'.format(tpc_task, core, count))
                 if core == "all":
                     total = count
                 else:
@@ -128,16 +140,17 @@ class TestTPCCores(Tester):
         debug(counts)
         debug(total)
 
-        self.assertTrue(self._is_balanced_distribution(total, cores, counts, op, phase))
+        self.assertTrue(TestTPCCores._is_balanced_distribution(total, cores, counts, tpc_task, phase))
         return out
 
-    def _run_phase(self, node, op, phase, last_node_stats=None, run_stress=True, op_count=default_op_count):
-        node_stats = self._stress_and_check_balance(node, op, phase, run_stress, op_count)
+    def _run_phase(self, node, tpc_task, phase, last_node_stats=None, stress_op=False, op_count=default_op_count):
+        debug(os.linesep + '=== ' + phase + ' ===')
+        node_stats = self._stress_and_check_balance(node, tpc_task, phase, stress_op, op_count)
         last_op_count = 0
         if last_node_stats is not None:
-            last_op_count = self._get_completed(last_node_stats, op)
+            last_op_count = TestTPCCores._get_all_completed(last_node_stats, tpc_task)
         expected_op_count = last_op_count + op_count / len(self.cluster.nodelist())
-        self._assert_count_expected(op, self._get_completed(node_stats, op), expected_op_count, phase)
+        self._assert_count_expected(tpc_task, TestTPCCores._get_all_completed(node_stats, tpc_task), expected_op_count, phase)
         return node_stats
 
     def tpc_ring_change_balance_cores_test(self):
@@ -151,44 +164,40 @@ class TestTPCCores(Tester):
         @jira_ticket APOLLO-694
         """
 
+        debug("Creating and starting a single-node cluster")
         cluster = self.cluster
-
         tokens = cluster.balanced_tokens(2)
         cluster.set_configuration_options(values={'num_tokens': 1})
-
-        debug("[node1, node2] tokens: %r" % (tokens,))
-
-        # Create a single node cluster
         cluster.populate(1)
         node1 = cluster.nodelist()[0]
         node1.set_configuration_options(values={'initial_token': tokens[0]})
-        cluster.start()
+        cluster.start(wait_for_binary_proto=True)
 
         # Do some initial loading to make sure it's balanced
         phase = 'initial write'
-        node1_stats = self._run_phase(node1, write_op, phase)
+        node1_stats = self._run_phase(node1, tpc_write_task, phase, stress_op='write')
 
-        # Add node
+        debug(os.linesep + "Adding a new node to the cluster and rebalancing the ring")
         node2 = new_node(cluster)
         node2.set_configuration_options(values={'initial_token': tokens[1]})
         node2.start(wait_for_binary_proto=True)
 
         # Check that read requests are properly distributed after node addition, even without flush
         phase = 'read on node1 after node addition'
-        node1_stats = self._run_phase(node1, read_op, phase, node1_stats)
+        node1_stats = self._run_phase(node1, tpc_read_task, phase, node1_stats, 'read')
         phase = 'check reads on node2 after read on node1'
-        node2_stats = self._run_phase(node2, read_op, phase, None, False)
+        node2_stats = self._run_phase(node2, tpc_read_task, phase)
 
         node1.flush()
 
         # Check that read requests are properly distributed after flush, both across cores and across nodes
         phase = 'read on node2'
-        node2_stats = self._run_phase(node2, read_op, phase, node2_stats)
+        node2_stats = self._run_phase(node2, tpc_read_task, phase, node2_stats, 'read')
         phase = 'check reads on node1 after flush and read on node2'
-        node1_stats = self._run_phase(node1, read_op, phase, node1_stats, False)
+        node1_stats = self._run_phase(node1, tpc_read_task, phase, node1_stats)
 
         # Check that write requests are properly distributed after flush, both across cores and across nodes
         phase = 'write on node1 after flush'
-        node1_stats = self._run_phase(node1, write_op, phase, node1_stats, True, 10000)
+        node1_stats = self._run_phase(node1, tpc_write_task, phase, node1_stats, 'write', 10000)
         phase = 'check writes on node2 after write on node1'
-        node2_stats = self._run_phase(node2, write_op, phase, node2_stats, False, 10000)
+        node2_stats = self._run_phase(node2, tpc_write_task, phase, node2_stats, False, 10000)
